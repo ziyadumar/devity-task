b92fd4ab31b9bb15fe6c847524f764c93a3d10ab
Ziyad Umar <ziyad@fantacode.com>
2021-09-05 00:48:46 +0200

    add color codes for card top-border

---
+++ src/app/core/values/color.schema.ts
@@ -0,0 +1,6 @@
+export enum ColorSchema {
+  facebook = 'hsl(208, 92%, 53%)',
+  instagram = 'linear-gradient(90deg, hsl(37, 97%, 70%), hsl(329, 70%, 58%))',
+  youtube = 'hsl(348, 97%, 39%)',
+  twitter = 'hsl(203, 89%, 53%)'
+}
