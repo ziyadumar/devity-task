d69fbc9bf35047850e587aa7f581f55cdda1ee98
Ziyad Umar <ziyad96@gmail.com>
2021-09-08 01:19:29 +0200

    Refoldering of components & Model Renaming of Cards' Component

--- src/app/core/interfaces/card.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Card {
-  platform: string;
-  icon: string;
-  username: string;
-  count: string;
-  isProgress: boolean;
-  percentage: number;
-}
--- src/app/core/interfaces/details.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface Detail {
-  id: number;
-  text: string;
-  icon: string;
-  count: string;
-  isProgress: boolean;
-  percentage: number;
-}
